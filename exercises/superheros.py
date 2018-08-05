de4033492983a3c7c9019a710c26953426c1e250
aboud20-meet <aboud20@meet.mit.edu>
2018-08-05 18:37:07 +0300

    good

--- exercises/superheros.py
+++ exercises/superheros.py
@@ -1,1 +1,19 @@
 # Write your solutions for 1.5 here!
+class Superheros:
+    def __init__(self, name, superpower, strength):
+        self.name = name
+        self.superpower = superpower
+        self.strength = strength
+    def name_strength(self):
+        print("My name is", self.name)
+        print("My strength is", self.strength)
+
+    def save_civilian(self, work):
+    
+        if work > self.strength:   
+            print ("Superhero is not strong enough! :(")
+            return
+        self.strength = self.strength - work
+Noor = Superheros ("Noor", "fly", 100)
+Noor.name_strength()
+Noor.save_civilian(99)
--- exercises/while_loops.py
+++ exercises/while_loops.py
@@ -1,1 +1,8 @@
 # Write your solution for 1.3 here!
+n = 1
+sum = 0
+while sum < 10000:
+    sum+=n
+    n+=2
+print (sum)    
+print (n)
